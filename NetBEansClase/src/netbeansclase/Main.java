dc3bb41a2a4ea78ffcb32c770280fa2394f55731
mirope <mirope@campusaula.com>
2014-09-16 09:38:27 +0200

    Update Main.java

--- NetBEansClase/src/netbeansclase/Main.java
+++ NetBEansClase/src/netbeansclase/Main.java
@@ -15,6 +15,12 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+     
+    public void saludar(){
+        System.out.println("hola");
+    
+    }
+     
     public static void main(String[] args) {
         // TODO code application logic here
     }
